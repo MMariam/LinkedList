b16e11f2592dfac9c6d406114f19a16097482d94
Mary H Abgarian <m.mariam.cs@gmail.com>
2015-10-08 06:50:52 -0700

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,44 +1,85 @@
-#include <iostream>
-#include <ctime>
-#include "Labs.h"
+#include <QtCore/QCoreApplication>
+#include "node.h"
+#include<iostream>
+//define preconditions
+
+void InsertHead(node* &head, int thing);//insert a head node
+void InsertAfter(node* &head, int num);//insert node with number after "head"
+node* Search(node*head, int thing);//search and return the node with the thing
+node* Remove(node* &head, node* here);//disconnect the node from the linked list
+node* Previous(node* head, node* here);//return the node before the "here" node
+void Delete(node* &head, node* here);//delete the memory allocated for "here"
+void Print(node* head);//print the damn thing
 
 
-/*
- THIS PROGRAM WILL BE ABLE TO CONSTRUCT A LIST, SORT IT, MERGE IT, AND REVERSE IT.
- */
 
-using namespace std;
-void TestList();
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
 
-int main (int argc, char * const argv[]) {
-
-		
-	Labs CLC;
-	station J("Jacob");
-	cout<<J;
-	CLC.SignIn(J,1);
-
-//TEST
-//	CLC.Print();	
-//	Labs<string > CLA;
-//	CLA.SignIn("Nick");
-//	CLA.SignIn("Michael");
-//	CLA.SignIn("Mary");
-//	CLA.Print();
-//	
-//	Labs<string > CLB;
-//	CLB.SignIn("Ashley");
-//	CLB.SignIn("Mary");
-//	CLB.Print();
-//	
-//	
-//	List<Labs<string> >A;
-//	A.InsertHead(CLC);
-//	A.Print();
-	
-	
-	
-	
-	return 0;
+    return a.exec();
 }
 
+
+
+void InsertHead(node* &head, int thing)//insert a head node
+{
+node* temp = new node;//create a new node
+
+temp->item=thing;
+temp->next=head;
+head=temp;
+}
+
+void InsertAfter(node* &head, int num)//insert node with number after "head"
+{
+node* thing=new node;
+thing->item=num;
+thing->next=head->next;
+head->next=thing;
+}
+
+node* Search(node*head, int thing)//search and return the node with the thing
+{
+bool found=false;
+node*it;
+for(it=head; !found && it!=NULL; it=it->next)
+if (it->item==thing)
+found=true;
+return it;
+}
+
+node* Remove(node* &head, node* here)//disconnect the node from the linked list
+{
+ node* p;
+if(here!=NULL)
+p=Previous(head, here);
+p->next=here->next;
+return p;
+}
+
+node* Previous(node* head, node* here)//return the node before the "here" node
+{
+node* it=head;
+if(here==head)
+return NULL;
+while(it->next!=here)
+it=it->next;
+return it;
+}
+
+void Delete(node* &head, node* here)//delete the memory allocated for "here"
+{
+node* temp=Remove(head, here);
+delete temp;
+}
+
+
+void Print(node* head)//print the damn thing
+{
+while(head!=NULL)
+{
+cout<<head->item<<" ";
+head=head->next;
+}
+}
